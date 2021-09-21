f5033b1de1e575b681b2bb3660658ffa363a5942
nguyen-william93 <=>
2021-09-20 21:28:54 -0500

    update state obj (still got more) and add function getStateID to grab the state id in the URL

--- assets/js/state.js
+++ assets/js/state.js
@@ -29,10 +29,201 @@ var stateID = [
         ID: "CO"
     },
     {
-
+        name: "CONNECTICUT",
+        captial: "",
+        ID: "CT"
     },
+    {
+        name: "DELAWARE",
+        capital: "",
+        ID: "DE"
+    },
+    {
+        name: "DISTRICT OF COLUMBIA",
+        capital: "",
+        ID: "DC"
+    },
+    {
+        name: "FLORIDA",
+        capital: "",
+        ID: "FL"
+    },
+    {
+        name: "GEORGIA",
+        capital: "",
+        ID: "GA"
+    },
+    {
+        name: "HAWAII",
+        capital: "",
+        ID: "HI"
+    },
+    {
+        name: "IDAHO",
+        capital: "",
+        ID: "ID"
+    },
+    {
+        name: "ILLINOIS",
+        capital: "",
+        ID: "IL"
+    },
+    {
+        name: "INDIANA",
+        captial: "",
+        ID: "IN"
+    },
+    {
+        name: "IOWA",
+        capital: "",
+        ID: "IA"
+    },
+    {
+        name: "KANSAS",
+        capital: "",
+        ID: "KS"
+    },
+    {
+        name: "LOUISIANA",
+        capital: "",
+        ID: "LA"
+    },
+    {
+        name: "MAINE",
+        capital: "",
+        ID: "ME"
+    },
+    {
+        name: "MARYLAND",
+        captial: "",
+        ID: "MD"
+    },
+    {
+        name: "MASSACHUSETTS",
+        captial: "",
+        ID: "MA"
+    },
+    {
+        name: "MICHIGAN",
+        captial: "",
+        ID: "MI"
+    },
+    {
+        name: "MINNESOTA",
+        captial: "",
+        ID: "MN"
+    },
+    {
+        name: " MISSISSIPPI",
+        captial: "",
+        ID: "MS"
+    },
+    {
+        name: "MISSOURI",
+        capital: "",
+        ID: "MO"
+    },
+    {
+        name: "MONTANA",
+        captial: "",
+        ID: "MT"
+    },
+    {
+        name: "NEBRASKA",
+        capital: "",
+        ID: "NE"
+    },
+    {
+        name: "NEVADA",
+        capital: "",
+        ID: "NV"
+    },
+    {
+        name: "NEW HAMPSHIRE",
+        capital: "",
+        ID: "NH"
+    },
+    {
+        name: "NEW JERSEY",
+        capital: "",
+        ID: "NJ"
+    },
+    {
+        name: "NEW YORK",
+        capital: "",
+        ID: "NY"
+    },
+    {
+        name: "NEW MEXICO",
+        captial: "",
+        ID: "NM"
+    },
+    {
+        name: "NORTH CORLINA",
+        capital: "",
+        ID: "NC"
+    },
+    {
+        name: "NORTH DAKOTA",
+        capital: "",
+        ID: "ND"
+    },
+    {
+        name: "OHIO",
+    },
+    {
+        name: "OKLAHOMA",
+    },
+    {
+        name: "OREGON",
+    },
+    {
+        name: "PENNYSLVANIA"
+    },
+    {
+        name: "RHODE ISLAND",
+    },
+    {
+        name: "SOUTH CAROLINA",
+    },
+    {
+        name: "SOUTH DAKOTA",
+    },
+    {
+        name: "TENNESSEE",
+    },
+    {
+        name: "TEXAS",
+    },
+    {
+        name: "UTAH",
+    },
+    {
+        name: "VERMOT",
+    },
+    {
+        name: "VIRGINIA",
+    },
+    {
+        name: "WASHINGTON",
+    },
+    {
+        name: "WEST VIRGINIA",
+    },
+    {
+        name: "WISCONSIN",
+    },
+    {
+        name: "WYOMING",
+    }
 
 ]
+
+var getStateID = function(){
+    var queryString = document.location.search;
+    var state = decodeURI(queryString).split("=")[1];
+    console.log(state);
+}
 
 var displayStateDataCovid = function(confirmed, death, infectionRate, population, transmissionLevel, ICU_bed){ //pass in whatever we want to be display
     //display the data by appending it
@@ -73,11 +264,12 @@ var getCityInfo = function(state){
 var getCovidInfo = function(){
     var apiUrl = "https://api.covidactnow.org/v2/states.json?apiKey=6cace70212de4ea3b58aea0276c7232e"
 
-    var response = fetch(apiUr).then(function(response){
+    var response = fetch(apiUrl).then(function(response){
         if (response.ok){
             response.json().then(function(data){
                 console.log(data);
                 displayStateDataCovid()//pass in the info we need
+                getState();
             })
         } else {
             console.log("error");
@@ -86,6 +278,8 @@ var getCovidInfo = function(){
 }
 
 //start function
+getState();
+getCovidInfo();
 
 
 
